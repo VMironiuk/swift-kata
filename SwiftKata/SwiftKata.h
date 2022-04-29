2f64ac329473d9d6c6197223e9109b28ec2f3197
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-04-29 18:05:11 +0300

    Add an empty project

---
+++ SwiftKata/SwiftKata.h
@@ -0,0 +1,18 @@
+//
+//  SwiftKata.h
+//  SwiftKata
+//
+//  Created by Vladimir Mironiuk on 29.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftKata.
+FOUNDATION_EXPORT double SwiftKataVersionNumber;
+
+//! Project version string for SwiftKata.
+FOUNDATION_EXPORT const unsigned char SwiftKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKata/PublicHeader.h>
+
+
