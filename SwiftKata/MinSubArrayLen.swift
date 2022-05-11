865164344a35606beab25ceab2d27894bdbbef19
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-05-11 18:44:01 +0300

    Add teacher's solution for MinSubArrayLen

--- SwiftKata/MinSubArrayLen.swift
+++ SwiftKata/MinSubArrayLen.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 public func minSubArrayLen(input: [Int], sum: Int) -> Int {
+    teacherSolution(input: input, sum: sum)
+}
+
+// MARK: - My Solution
+
+private func mySolution(input: [Int], sum: Int) -> Int {
     var slidingWindowLength = 0
     while slidingWindowLength < input.count {
         var tempSum = 0
@@ -30,4 +36,32 @@ public func minSubArrayLen(input: [Int], sum: Int) -> Int {
     }
     
     return .zero
+}
+
+// MARK: - Teacher's Solution
+
+private func teacherSolution(input: [Int], sum: Int) -> Int {
+    var total = 0
+    var start = 0
+    var end = 0
+    var minLength = Int.max
+    
+    while start < input.count {
+        // if current window doesn't add up to the given sum then
+        // move the window to right
+        if total < sum && end < input.count {
+            total += input[end]
+            end += 1
+        // if current window adds up to at least the sum given then
+        // we can shrink the window
+        } else if total >= sum {
+            minLength = min(minLength, end - start)
+            total -= input[start]
+            start += 1
+        } else {
+            break
+        }
+    }
+    
+    return minLength == Int.max ? 0 : minLength
 }
