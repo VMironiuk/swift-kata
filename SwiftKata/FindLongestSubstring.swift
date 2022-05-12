55e5a4cac8581db212579dbf3f119de42670e74e
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-05-12 18:14:15 +0300

    Add teacher solution for FindLongestSubstring

--- SwiftKata/FindLongestSubstring.swift
+++ SwiftKata/FindLongestSubstring.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 public func findLongestSubstring(_ string: String) -> Int {
+    teacherSolution(string)
+}
+
+// MARK: - My Solution
+
+private func mySolution(_ string: String) -> Int {
     guard !string.isEmpty else {
         return .zero
     }
@@ -33,4 +39,25 @@ public func findLongestSubstring(_ string: String) -> Int {
     }
     
     return slidingWindowLength
+}
+
+// MARK: - Teacher's Solution
+
+private func teacherSolution(_ string: String) -> Int {
+    var longest = 0
+    var seen = [Character: Int]()
+    var start = 0
+    
+    for i in 0..<string.count {
+        let char = string[string.index(string.startIndex, offsetBy: i)]
+        if let index = seen[char] {
+            start = max(start, index)
+        }
+        // index - beginning of substring + 1 (to include current in count)
+        longest = max(longest, i - start + 1)
+        // store the index of the next char so as to not double count
+        seen[char] = i + 1
+    }
+    
+    return longest
 }
